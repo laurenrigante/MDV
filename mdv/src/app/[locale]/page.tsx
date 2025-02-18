aa39f7911dfb5031a14ced0cfe2b29e10a0fe059
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-18 16:07:31 -0500

    starting layout of homepage #3

--- mdv/src/app/[locale]/page.tsx
+++ mdv/src/app/[locale]/page.tsx
@@ -9,8 +9,27 @@ export default function HomePage() {
   return (
     <div>
       <Navbar />
+
+      <div
+        className="relative w-full h-[60vh] overflow-hidden bg-fixed bg-center bg-cover"
+        style={{ backgroundImage: "url('/grass.jpg')" }}
+      >
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="w-[50%] min-h-[200px] bg-white bg-opacity-70 px-8 py-6 rounded-md shadow-lg">
+            <h1 className="text-black text-2xl md:text-4xl lg:text-5xl font-bold text-left mb-4">
+              Commercial Landscaping and Snow Removal
+            </h1>
+            <h3 className="text-black text-lg md:text-xl text-left">
+              With XX years of experience, MDV ensures top-quality service for
+              all your landscaping and snow removal needs.
+            </h3>
+          </div>
+        </div>
+      </div>
+
       <h1>{t("title")}</h1>
       <Link href="/contact">{t("contact")}</Link>
+      <div className="h-[1000px]"></div>
     </div>
   );
 }
