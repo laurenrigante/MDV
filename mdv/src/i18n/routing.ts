e4145561e388c165ccd0f98ae7da3324f926303c
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-18 10:04:18 -0500

    SEO optimization- url translation #1

--- mdv/src/i18n/routing.ts
+++ mdv/src/i18n/routing.ts
@@ -1,15 +1,22 @@
-import {defineRouting} from 'next-intl/routing';
-import {createNavigation} from 'next-intl/navigation';
- 
+import { defineRouting } from 'next-intl/routing';
+import { createNavigation } from 'next-intl/navigation';
+
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['en', 'fr'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
-});
- 
-export type Locale= (typeof routing.locales)[number];
 
-export const {Link, redirect, usePathname, useRouter, getPathname} =
+  // Used when no locale matches
+  defaultLocale: 'en',
+
+  pathnames: {
+    "/contact": {
+      en: "/contact-us",
+      fr: "/contactez-nous",
+    }
+  }
+});
+
+export type Locale = (typeof routing.locales)[number];
+
+export const { Link, redirect, usePathname, useRouter, getPathname } =
   createNavigation(routing);
