bb12ef5d75ef99919ca2f8c8834f19bae3d1d7bb
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-18 11:10:55 -0500

    General CSS and colors+font added

--- mdv/tailwind.config.ts
+++ mdv/tailwind.config.ts
@@ -11,8 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryGreen: '#8CC63F', // Green
+        primaryBlue: '#00AEEF', // Blue
+        primaryGray: '#6D6E71', // Gray 
+        secondaryBlue: '#0A122A',// oxford blue
+        secondaryGreen: '#475841', //feldgrau
+        tertiaryGreen: '#507255', //hookers green
+        primaryGreen2: '#628D2A', //4 shades darkrerer than primary
+        primaryBlue2:'#0078A2', //4 shades darkerr than prrimary
+        secondaryBlack:"#161925", //raisin black
+        silver: "#c5baaf", //silver
+        secondaryWhite:"FFFCF7" //baby powder
+                 
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 } satisfies Config;
