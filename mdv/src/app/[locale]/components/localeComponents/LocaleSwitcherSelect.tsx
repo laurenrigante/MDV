3605b29c843fc2b79423506eb88104e1e94e03fd
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-18 15:34:24 -0500

    wip --> styling of navbar #2

--- mdv/src/app/[locale]/components/localeComponents/LocaleSwitcherSelect.tsx
+++ mdv/src/app/[locale]/components/localeComponents/LocaleSwitcherSelect.tsx
@@ -34,16 +34,15 @@ export default function LocaleSwitcherSelect({ defaultValue, label }: Props) {
 
   return (
     <Select defaultValue={defaultValue} onValueChange={onSelectChange}>
-      
       <SelectTrigger
-        className='w-[100px] h-8 border-none bg-transparent focus:ring-0 focus:ring-offset-0'
+        className='w-[100px] h-8 border-primaryGray'
         aria-label={label}
       >
       <SelectValue />
       </SelectTrigger>
       <SelectContent>
         {routing.locales.map((locale) => (
-          <SelectItem key={locale} value={locale}>
+          <SelectItem key={locale} value={locale} className="bg-white">
             {locale.toUpperCase()}
           </SelectItem>
         ))}
