c3a87cb4a230e2e0641c8db63759b0bfdb32bba3
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-22 13:27:23 -0500

    wip--> Career page: general layout, form, including a file, validation, toasts, english and french language #6

--- mdv/src/app/[locale]/careers/page.tsx
+++ mdv/src/app/[locale]/careers/page.tsx
@@ -1,13 +1,291 @@
-'use client';
-import { useTranslations } from "next-intl";
+"use client";
 import Navbar from "../components/Navbar";
+import { useTranslations } from "next-intl";
+import Footer from "../components/footer";
+import { motion } from "framer-motion";
+import {
+  Building,
+  CircleHelpIcon,
+  Phone,
+  File,
+  UploadCloud,
+} from "lucide-react";
+import GoogleMaps from "../components/GoogleMaps";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
-export default function ContactPage() {
-  const t = useTranslations("NavbarLinks");
+export default function CareerPage() {
+  const t = useTranslations("CareerPage");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [files, setFiles] = useState<File[]>([]);
+
+  // Handle form submission
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validate fields
+    if (!fullname || !email || !message ||!phone) {
+      toast.error("Please fill in all required fields (*)");
+      return;
+    }
+
+    // Phone number validation (xxx-xxx-xxxx or xxxxxxxxxx)
+    const phoneRegex =
+      /^[2-9]{1}[0-9]{2}-[0-9]{3}-[0-9]{4}$|^[2-9]{1}[0-9]{9}$/;
+    if (!phoneRegex.test(phone)) {
+      toast.error(
+        "Please provide a valid 10-digit phone number : xxx-xxx-xxxx"
+      );
+      return;
+    }
+
+    if (files.length < 1) {
+      toast.error("Please upload at least 1 file.");
+      return;
+    }
+
+    if (files.length > 3) {
+      toast.error("Maximum 3 files allowed.");
+      return;
+    }
+
+    toast.success("Form submitted successfully!");
+    //use resend to send this to mikes work email address
+
+    // Reset form fields after submission
+    setFullname("");
+    setEmail("");
+    setMessage("");
+    setPhone("");
+    setFiles([]); // Reset files array
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSubmit(event); // Trigger login on Enter key press
+    }
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    selectedFiles.forEach((file) => {
+      if (file.size <= 10 * 1024 * 1024) {
+        setFiles((prev) => [...prev, file]); // Add file if within size limit
+      } else {
+        console.error(`File "${file.name}" exceeds 10MB limit.`);
+        toast.error(`File "${file.name}" exceeds 10MB limit.`);
+      }
+    });
+  };
+
+  const handleRemoveFile = (index: number) => {
+    setFiles((prev) => prev.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>{t("careers")}</h1>
+
+      <div
+        className="relative w-full h-[50vh] overflow-hidden bg-fixed bg-bottom bg-cover mt-[60px]"
+        style={{ backgroundImage: "url('lawn.jpg')" }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-40"
+        >
+          <div className="w-[50%] min-h-[100px] h-auto bg-white bg-opacity-70 px-8 py-6 rounded-md shadow-lg">
+            <h1 className="text-black text-2xl md:text-4xl lg:text-5xl font-bold text-center">
+              {t("title")}
+            </h1>
+          </div>
+        </motion.div>
+      </div>
+
+      <section className="p-10 bg-primaryGray/40">
+        <div className="flex flex-col  md:flex-row xl:flex-row 2xl:flex-row justify-around ">
+          <div className=" mb-5 md:w-1/2 md:mr-10 lg:w-1/2 lg:mr-10 xl:w-1/2 xl:mr-10 2xl:w-1/2 2xl:mr-10">
+            <div className="flex gap-3">
+              <CircleHelpIcon className="text-primaryGreen2 w-10 h-10" />
+              <h3 className="text-black font-medium text-2xl text-center mb-10">
+                {t("subtitle")}
+              </h3>
+            </div>
+            <p className="font-medium text-xl text-justify "> {t("desc")}</p>
+          </div>
+
+          <form
+            role="form"
+            className="flex flex-col border-[3px] border-primaryGreen2 rounded-md p-5 bg-white gap-3"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col  w-full">
+              <label
+                htmlFor="fname"
+                className="font-medium text-lg text-primaryGreen2"
+              >
+                {" "}
+                {t("fullname")} *
+              </label>
+              <input
+                type="text"
+                id="fname"
+                value={fullname}
+                className=" py-1 pl-2 bg-primaryGreen2/30 rounded-md "
+                onChange={(e) => setFullname(e.target.value)}
+                onKeyDown={handleKeyDown}
+                aria-required="true"
+              ></input>
+            </div>
+
+            <div className="flex flex-col sm:flex-row md:flex-row gap-3">
+              <div className="flex flex-col  w-full ">
+                <label
+                  htmlFor="email"
+                  className="font-medium text-lg text-primaryGreen2"
+                >
+                  {" "}
+                  {t("email")} *
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  className=" py-1 pl-2 bg-primaryGreen2/30 rounded-md"
+                  onChange={(e) => setEmail(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                  aria-required="true"
+                ></input>
+              </div>
+              <div className="flex flex-col w-full ">
+                <label
+                  htmlFor="tel"
+                  className="font-medium text-lg text-primaryGreen2"
+                >
+                  {" "}
+                  {t("phone")} *
+                </label>
+                <input
+                  type="tel"
+                  id="tel"
+                  value={phone}
+                  className=" py-1 pl-2 bg-primaryGreen2/30 rounded-md"
+                  onChange={(e) => setPhone(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                ></input>
+              </div>
+            </div>
+            <label
+              htmlFor="msg"
+              className="font-medium text-lg text-primaryGreen2"
+            >
+              {" "}
+              {t("msg")} *
+            </label>
+            <textarea
+              className=" py-1 pl-2 bg-primaryGreen2/30 rounded-md"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+            ></textarea>
+
+            {/* File Upload */}
+            <div>
+              <label
+                htmlFor="file-upload"
+                className="font-medium text-lg text-primaryGreen2"
+              >
+                CV *
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-300 px-2 py-5">
+                <div className="text-center">
+                  <UploadCloud className="mx-auto h-12 w-12 text-gray-300" />
+                  <label
+                    htmlFor="file-upload"
+                    className="relative mt-4 inline-block rounded-md bg-primaryGreen px-4 py-2 text-sm font-medium text-white shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-[#7baf33] focus:outline-none focus:ring-2 focus:ring-[#8CC63F] focus:ring-offset-2 cursor-pointer"
+                  >
+                    {t("uploadfiles")}
+                    <input
+                      id="file-upload"
+                      type="file"
+                      multiple
+                      onChange={handleFileChange}
+                      className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                    />
+                  </label>
+
+                  {/* Display Selected Files */}
+                  {!!files.length && (
+                    <div className="mt-4 text-left">
+                      <p className="text-sm font-medium text-primaryGray mb-2">
+                        {t("attached")}:
+                      </p>
+                      <ul className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
+                        {files.map((file, idx) => {
+                          return (
+                            <li
+                              key={`${file.name}-${idx}`}
+                              className="relative flex items-center gap-3 rounded-md border border-gray-200 bg-white p-3 shadow-sm"
+                            >
+                              <button
+                                type="button"
+                                onClick={() => handleRemoveFile(idx)}
+                                className="absolute top-1 right-1 rounded-full bg-white text-gray-400 hover:text-red-600"
+                              >
+                                ✕
+                              </button>
+                              <File className="h-8 w-8 text-gray-300" />
+
+                              <span className="text-sm text-gray-600 truncate max-w-[8rem]">
+                                {file.name}
+                              </span>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              role="button"
+              className="bg-[#8CC63F]  mt-5 text-white text-xl font-medium rounded-lg px-6 py-3 shadow-md transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-[#7baf33] focus:outline-none focus:ring-2 focus:ring-[#8CC63F] focus:ring-offset-2 mb-5"
+            >
+              {" "}
+              {t("submit")}
+            </button>
+          </form>
+        </div>
+      </section>
+
+      <section className="bg-white p-10">
+        <div className="w-full mx-auto text-center flex flex-col justify-center items-center gap-2">
+          <h2 className="text-2xl font-bold mb-4 border-b-4 border-primaryGreen inline-block pb-1 px-2">
+            {t("waysToReach")}
+          </h2>
+
+          <div className="flex items-center">
+            <Phone className="mr-3 text-primaryGreen text-sm" />
+            <span>(514) 766-5386</span>
+          </div>
+          <div className="flex items-center mb-3">
+            <Building className="mr-3 text-primaryGreen text-sm" />
+            <span>8501 Rue Cordner, Lasalle, QC</span>
+          </div>
+
+          <GoogleMaps />
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 }
