855393ae589941060d8350673560cf04840e6545
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-23 17:22:53 -0500

    automatic image carousel component

---
+++ mdv/src/app/[locale]/components/ImageCarousel.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+
+interface CarouselProps{
+    images: string[];
+}
+
+export default function ImageCarousel( {images}:CarouselProps) {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 6000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative  md:w-[400px] md:h-[300px]  lg:w-[800px] lg:h-[500px] overflow-hidden rounded-lg shadow-lg">
+      <AnimatePresence mode="wait">
+        <motion.img
+          key={images[index]}
+          src={images[index]}
+          alt="Sliding Image"
+          className="w-full h-full object-cover rounded-lg"
+          initial={{ opacity: 0, scale: 1.1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 1.1 }}
+          transition={{ duration: 0.8 }}
+        />
+      </AnimatePresence>
+    </div>
+  );
+}
