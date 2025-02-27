3b8cd60bf25d8537b247000bea69e3435b4d6705
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-27 17:16:13 -0500

    changing image asset for looking for a job section

--- mdv/src/app/[locale]/page.tsx
+++ mdv/src/app/[locale]/page.tsx
@@ -160,10 +160,13 @@ export default function HomePage() {
       </motion.section>
 
       <section className="relative w-full h-[60vh] sm:h-[40vh] md:h-[40vh] lg:h-[40vh] xl:h-[40vh] 2xl:h-[40vh] shadow-2xl mb-8">
-        <div
-          className="w-1/2 h-full bg-cover bg-center absolute left-0 top-0 hidden sm:block"
-          style={{ backgroundImage: "url('/mdvemp.jpg')" }}
-        ></div>
+        <div className="w-1/2 h-full bg-cover bg-center absolute left-0 top-0 hidden sm:block">
+          <img
+            src="/mdvemp.jpg"
+            alt="background"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        </div>
 
         <div className="w-full sm:w-1/2 h-full bg-white px-8 py-6 flex flex-col justify-center items-center absolute right-0 top-0">
           <div className="text-left">
