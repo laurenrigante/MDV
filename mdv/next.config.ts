b7340a50ad7312c680aabc666527f956d74a9866
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-27 18:36:30 -0500

    favicon set

--- mdv/next.config.ts
+++ mdv/next.config.ts
@@ -4,7 +4,9 @@ import type { NextConfig } from "next";
 const withNextIntl=createNextIntlPlugin();
 
 
-const nextConfig= {};
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+  };
 
 
 export default withNextIntl(nextConfig);
--- mdv/src/app/[locale]/layout.tsx
+++ mdv/src/app/[locale]/layout.tsx
@@ -1,27 +1,30 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {notFound} from 'next/navigation';
-import {Locale, routing} from '@/i18n/routing';
-import 'react-toastify/dist/ReactToastify.css'; 
-import { ToastContainer } from 'react-toastify'; 
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { Locale, routing } from "@/i18n/routing";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 export default async function LocaleLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }) {
-  const {locale} = await params;
+  const { locale } = await params;
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
   }
- 
+
   // Providing all messages to the client side is the easiest way to get started
   const messages = await getMessages();
- 
+
   return (
     <html lang={locale}>
+      <head>
+        <link rel="icon" href="/favicon-16x16.png" />
+      </head>
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
