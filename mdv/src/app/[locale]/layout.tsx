3244e49f46c8cce1cb61e91d7d3bb2080cf734c8
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-02-28 17:41:39 -0500

    wip--> opengraph  metadata change

--- mdv/src/app/[locale]/layout.tsx
+++ mdv/src/app/[locale]/layout.tsx
@@ -4,9 +4,10 @@ import { notFound } from "next/navigation";
 import { Locale, routing } from "@/i18n/routing";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 export const metadata = {
-  title: "MDV",
+  title: "MDV: Paysagement |  Déneigement | Commercial ",
   description: "Paysagement et Déneigement Commercial",
   openGraph: {
     title: "MDV",
@@ -44,9 +45,36 @@ export default async function LocaleLayout({
   return (
     <html lang={locale}>
       <head>
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta
+          property="og:image:width"
+          content={metadata.openGraph.images[0].width.toString()}
+        />
+        <meta
+          property="og:image:height"
+          content={metadata.openGraph.images[0].height.toString()}
+        />
+        <meta
+          property="og:image:alt"
+          content={metadata.openGraph.images[0].alt}
+        />
+        <meta property="og:type" content={metadata.openGraph.type} />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
       </head>
       <body>
         <NextIntlClientProvider messages={messages}>
