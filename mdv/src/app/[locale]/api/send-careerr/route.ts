675378a135754f34d3d979b0a5421639c6e016ab
lauren rigante <93160873+laurenrigante@users.noreply.github.com>
2025-03-11 12:55:18 -0400

    updating email

--- mdv/src/app/[locale]/api/send-careerr/route.ts
+++ mdv/src/app/[locale]/api/send-careerr/route.ts
@@ -123,7 +123,7 @@ export async function POST(req: Request) {
         );
         const { data, error } = await resend.emails.send({
             from: 'onboarding@resend.dev',
-            to: 'mike@paysagistemdv.com', 
+            to: 'lrigante@hotmail.com', 
             subject: 'New Career Inquiry Received',
             html: htmlContent,
             attachments,
